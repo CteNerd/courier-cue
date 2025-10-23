61257ea7b1c70da118266c2e4f2f07f4b58a1859
Roddy Tomlin <roddytdog@yahoo.com>
2025-10-23 10:09:45 -0500

    fix: add AuthProvider to enable Cognito OAuth

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './hooks/useAuth';
 import { UserProvider } from './hooks/useUser';
 import { ThemeProvider } from './hooks/useTheme';
 import App from './App';
@@ -23,9 +24,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <ThemeProvider>
-          <UserProvider>
-            <App />
-          </UserProvider>
+          <AuthProvider>
+            <UserProvider>
+              <App />
+            </UserProvider>
+          </AuthProvider>
         </ThemeProvider>
       </BrowserRouter>
     </QueryClientProvider>
